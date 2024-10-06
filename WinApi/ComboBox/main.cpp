3a74e3e9023152a3e4015c599896e5cd22c235a7
Nazarets Ksenia <ksenianazarec995@gmail.com>
2024-10-06 05:47:35 -0700

    idok for combo box defined

--- WinApi/ComboBox/main.cpp
+++ WinApi/ComboBox/main.cpp
@@ -24,7 +24,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
 		for (int i = 0; i < sizeof(g_COMBO_BOX_ITEMS) / sizeof(g_COMBO_BOX_ITEMS[0]); ++i)
 		{
-			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_COMBO_BOX_ITEMS[i]);
+			SendMessage(hCombo, CB_INSERTSTRING, i, (LPARAM)g_COMBO_BOX_ITEMS[i]);
 		}
 		SendMessage(hCombo, CB_SETCURSEL, 0, 0);
 		//CB - Combo box
@@ -33,9 +33,25 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}break;
 	case WM_COMMAND:
 	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			HWND hCB = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCB, CB_GETCURSEL,0,0);
+			CHAR buffer[256]{};
+			SendMessage(hCB, CB_GETLBTEXT, i, (LPARAM)buffer);
+			MessageBox(hwnd, buffer, "Info", MB_OK );
 
+		}break;
+		case IDCANCEL:
+		{
+			EndDialog(hwnd, 0);
+		}break;
 
+		}
 	}break;
+
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 		break;
